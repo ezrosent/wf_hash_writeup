a34b94690c0ce1ae5ad0963776020bcfbb3f0afc
Eli Rosenthal <ezr@google.com>
2017-01-30 22:44:33 -0800

    small updates

--- data/write_imgs.R
+++ data/write_imgs.R
@@ -16,3 +16,12 @@ gen_plot('high_90.csv', '95% Occupancy, 90% Lookups')
 gen_plot('medium_20.csv', '50% Occupancy, 20% Lookups')
 gen_plot('medium_90.csv', '50% Occupancy, 90% Lookups')
 gen_plot('grow_results.csv', 'Normalized Resize Overhead', ylab="")
+
+# ggplot(table, aes(table$num_threads)) +
+# geom_point(aes(y=wf_hash, colour="wf_hash", shape="wf_hash")) +
+# geom_line(aes(y=wf_hash, colour="wf_hash")) +
+# geom_point(aes(y=hopd, colour="hopd", shape="hopd")) +
+# geom_line(aes(y=hopd, colour="hopd")) +
+# geom_point(aes(y=chainmtm, colour="chainmtm", shape="chainmtm")) +
+# geom_line(aes(y=chainmtm, colour="chainmtm")) +
+# labs(x="Number of Threads", y="Operations/Second")
